3cd74737fe0a7eb4b0facc12aadb1e89848d93eb
ydatech <yudasukmana1992@gmail.com>
2020-03-10 15:16:57 +0700

    show

--- src/pages/private/chat/list.js
+++ src/pages/private/chat/list.js
@@ -7,9 +7,11 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Fab from '@material-ui/core/Fab';
 import AddChatDialog from './add';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import Divider from '@material-ui/core/Divider';
-
+import Badge from '@material-ui/core/Badge';
 import ChatIcon from '@material-ui/icons/Chat';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 
 import useStyles from './styles/list';
 
@@ -54,7 +56,15 @@ export default function ChatList() {
                         <ListItemAvatar>
                             <Avatar alt={profile.nama} src={profile.foto} className={classes.orange} />
                         </ListItemAvatar>
-                        <ListItemText primary={profile.nama} secondary={chat.last_message.text || 'Belum ada pesan, kirim sekarang!'} />
+                        <ListItemText primary={profile.nama} secondary={chat.is_typing && chat.is_typing[profile.id] ? 'mengetik...' : (chat.last_message.text || 'Belum ada pesan, kirim sekarang!')} />
+                        {chat.unread_count[user.uid] > 0 &&
+                            <ListItemSecondaryAction>
+                                <Badge color="primary" badgeContent={chat.unread_count[user.uid]}>
+                                    <ChatBubbleOutlineIcon />
+                                </Badge>
+                            </ListItemSecondaryAction>}
+
+
                     </ListItem>
                     <Divider />
                 </React.Fragment>
